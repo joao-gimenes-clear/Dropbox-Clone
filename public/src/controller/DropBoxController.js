0194924b957c6e9a565ae8d6cb81f99dc8821e33
joao.gimenes.clear <joao.gimenes.clear@gmail.com>
2023-02-23 09:57:09 -0300

    Feat: file selection using shift and ctrl

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -1,5 +1,7 @@
 class DropBoxController {
     constructor() {
+      this.onselectionchange = new Event('selectionchange');
+
       this.btnSendFileEl = document.querySelector("#btn-send-file");
       this.inputFilesEl = document.querySelector("#files");
       this.snackModalEl = document.querySelector("#react-snackbar-root");
@@ -29,6 +31,13 @@ class DropBoxController {
     
 
     initEvents() {
+
+      this.listFilesEl.addEventListener('selectionchange', e=>{
+
+        console.log('selectionchange');
+
+      })
+
       this.btnSendFileEl.addEventListener("click", (event) => {
 
         this.inputFilesEl.click();
@@ -340,6 +349,8 @@ class DropBoxController {
         <div class="name text-center">${file.originalFilename}</div>
       `;
 
+      this.initEventsLi(li);
+
        return li;
 
     }
@@ -355,11 +366,66 @@ class DropBoxController {
           let key = snapshotItem.key;
           let data = snapshotItem.val();
 
-          console.log(key, data)
-
           this.listFilesEl.appendChild(this.getFileView(data, key));
 
         })
+
+      })
+
+    }
+
+    initEventsLi(li){
+
+      li.addEventListener('click', e=>{
+
+        
+
+        this.listFilesEl.dispatchEvent(this.onselectionchange);
+
+        if(e.shiftKey) {
+
+          let firstLi = this.listFilesEl.querySelector('.selected');
+
+          if (firstLi) {
+            
+            let indexStart;
+            let indexEnd;
+            let lis = li.parentElement.childNodes
+
+            lis.forEach((el, index)=>{
+
+              if (firstLi === el) indexStart = index;
+              if (li === el) indexEnd = index;
+
+            });
+
+            let index = [indexStart, indexEnd].sort();
+
+            lis.forEach((el, i)=>{
+
+              if (i >= index[0] && i <= index[1]) {
+                el.classList.add('selected')
+              }
+
+            });
+
+            return true;
+
+          }
+
+        }
+
+        if (!e.ctrlKey) {
+
+          this.listFilesEl.querySelectorAll('li.selected').forEach(el=>{
+
+            el.classList.remove('selected');
+
+          })
+
+        }
+
+        li.classList.toggle('selected');
 
       })
 
